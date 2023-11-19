1ffb3494204bdfc8ac11bc931e30da9b47374d66
Roberta Vieira <139922545+robertasvieira@users.noreply.github.com>
2023-11-19 18:10:27 -0300

    Create rotas.py
    Código em Python que utiliza o framework Flask para criar duas rotas web.

---
+++ A01/rotas.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def homepage():
+return "Hello Web.py"
+
+@app.route("/start")
+
+def start():
+return "Starting..."
+
+app.run(debug=True)
